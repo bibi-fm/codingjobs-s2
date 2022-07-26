5b9c6043d7023af388a829804714edf9ad100e0d
Simon <fctinstean@gmail.com>
2022-07-26 16:57:58 +0200

    Cookie and session

---
+++ PHP/Day 4 - Cookies_Session_Include/Example files/03_sessions/session.php
@@ -0,0 +1,22 @@
+<?php
+
+// SESSIONS
+
+/* 
+First : you need to start the session.
+If it's the first time, the server creates a new session.
+If you already opened a session before, it will try to open this one.
+*/
+
+session_start();
+
+// Add an element to the session : 
+$_SESSION['cart'] = 'Tshirt';
+
+// Remove an element :
+// unset($_SESSION['cart']);
+
+echo '<pre>';
+var_dump($_SESSION);
+echo '</pre>';
+
