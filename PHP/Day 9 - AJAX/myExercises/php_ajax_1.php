332d9ee53483062e7789d8889dec973484ba9a4e
Brícia <102607591+bibi-fm@users.noreply.github.com>
2022-08-08 13:50:17 +0200

    add completed ajax exercise 1

---
+++ PHP/Day 9 - AJAX/myExercises/php_ajax_1.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+	-- Exercise : 
+
+	In this exercise, you will use the 'spotify' database.
+    You can reuse code from previous exercises.
+
+	- Step 1 :
+		Create two pages: 'songs.html' and 'get-songs.php' DONE
+    
+    - Step 2 : 
+        'get-songs.php' should query the database to get all the songs. DONE
+    
+    - Step 3 :
+        When you reach songs.html, this page should display all the songs.
+        To perfom this action, you must use AJAX.
+        AJAX should get the data from 'get-songs.php' and display it on the page.
+
+*/
