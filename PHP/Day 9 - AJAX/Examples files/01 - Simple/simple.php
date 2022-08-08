64ba6b0c5f7328c9f77d3ad8bfe647bd2685b133
Simon <fctinstean@gmail.com>
2022-08-08 11:45:10 +0200

    AJAX

---
+++ PHP/Day 9 - AJAX/Examples files/01 - Simple/simple.php
@@ -0,0 +1,9 @@
+<?php
+
+// Everything that you display/echo in this file will be retrieved in the 'result' variable of Javascript
+echo 'Hello';
+$array = array(10, 5, 9, 1, 5);
+var_dump($array);
+?>
+
+<p>Whatever</p>
