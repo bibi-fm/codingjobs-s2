bb972c3e93467e0f73cbd9676eec5422b1f27366
Brícia <102607591+bibi-fm@users.noreply.github.com>
2022-08-09 11:00:16 +0200

    add completed debug exercise

--- PHP/Day 9 - AJAX/myExercises/debug_1/get-songs.php
+++ PHP/Day 9 - AJAX/myExercises/debug_1/get-songs.php
@@ -1,18 +1,18 @@
 <?php
-
 $conn = mysqli_connect('localhost', 'root', '', 'spotify_db');
 $query = 'SELECT * FROM songs';
-$results = mysqli_query($conn, $queri);
-$songs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$results = mysqli_query($conn, $query);
+$songs = mysqli_fetch_all($results, MYSQLI_ASSOC);
 
-// Order by
 if (isset($_POST['orderby'])) {
     $query = 'SELECT * FROM songs ORDER BY title ' . $_POST['orderby'];
     $results = mysqli_query($conn, $query);
+    $songs = mysqli_fetch_all($results, MYSQLI_ASSOC);
 }
 
+
 foreach ($songs as $song) {
-    echo $songs['title'] . '<br>';
-    echo $songs['release_date'] . '<br>';
+    echo $song['title'] . '<br>';
+    echo $song['release_date'] . '<br>';
     echo '<hr>';
 }
