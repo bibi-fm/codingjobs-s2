51ed11269a9ab14fd04ef95f4d7311276bf2d3e9
Brícia <102607591+bibi-fm@users.noreply.github.com>
2022-08-19 14:14:26 +0200

    add corrected ajax exercise 3

--- PHP/Day 9 - AJAX/myExercises/songs_3/search-songs.php
+++ PHP/Day 9 - AJAX/myExercises/songs_3/search-songs.php
@@ -8,7 +8,6 @@ $results = mysqli_query($conn, $query);
 $songs = mysqli_fetch_all($results, MYSQLI_ASSOC);
 ?>
 <?php foreach ($songs as $song) : ?>
-    <h3><?php $song['title']?></h3>
-    <p><?php $song['release_date']?></p>
-    <p><?php $song['poster']?></p>
+    <h3><?= $song['title']?></h3>
+    <p><?= $song['release_date']?></p>
 <?php endforeach ?>
