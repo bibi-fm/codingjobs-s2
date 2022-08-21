2d67917326b9e0ac094d38851bbdc931fbc90f53
Brícia <102607591+bibi-fm@users.noreply.github.com>
2022-08-21 10:22:49 +0200

    add completed oop human exercise 0

---
+++ PHP - OOP/Day 1 - OOP Basics/myExercises/exercise0/oop_human.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+An human is represented by a name, an haircolor, a gender and a height. DONE
+
+Create the matching class. DONE
+The name should not be editable after creation of the object. DONE
+
+Create two different object from this class. DONE
+Display the two objects properties using tostring. DONE
+
+*/
