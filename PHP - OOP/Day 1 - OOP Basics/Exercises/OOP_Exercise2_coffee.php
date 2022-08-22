c75b7565798ee6ec9c0ffda0787ff282d2f04e5f
Simon <fctinstean@gmail.com>
2022-08-22 12:06:21 +0200

    coffee

--- PHP - OOP/Day 1 - OOP Basics/Exercises/OOP_Exercise2_coffee.php
+++ PHP - OOP/Day 1 - OOP Basics/Exercises/OOP_Exercise2_coffee.php
@@ -20,6 +20,42 @@
 	$myCoffee->setTemperature(65);
 */
 
+class CoffeeCup
+{
+	private $quantity;
+	private $brand;
+	private $temperature;
+
+	function __construct()
+	{
+	}
+
+	function setQuantity($q)
+	{
+		$this->quantity = $q;
+	}
+	function setBrand($b)
+	{
+		$this->brand = $b;
+	}
+	function setTemperature($t)
+	{
+		$this->temperature = $t;
+	}
+	function getQuantity()
+	{
+		return $this->quantity;
+	}
+	function getBrand()
+	{
+		return $this->brand;
+	}
+	function getTemperature()
+	{
+		return $this->temperature;
+	}
+}
+
 
 /*
 	Part 2 :
