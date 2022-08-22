af5121b70b57385b6b9fd450e1c3745c3b19e1a6
Simon <fctinstean@gmail.com>
2022-08-22 10:25:15 +0200

    solution exercise 1

--- PHP - OOP/Day 1 - OOP Basics/Example files/client.php
+++ PHP - OOP/Day 1 - OOP Basics/Example files/client.php
@@ -5,7 +5,7 @@ require_once 'Car.php';
 $ferrari = new Car('Red', 'Ferrari', 2018);
 $ferrari->set_color('Blue');
 
-echo $ferrari->get_color() . '<br>';
+//echo $ferrari->get_color() . '<br>';
 
 // Display the object (works only with tostring)
 echo $ferrari;
---
+++ PHP - OOP/Day 1 - OOP Basics/Exercises/Exercise_0/client.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'Human.php';
+
+$fabio = new Human('Fabio', 'Brown', 'Male', 1.7);
+$margo = new Human('Margo', 'Brown', 'Female', 1.7);
+
+echo $fabio;
+echo '<hr>';
+echo $margo;
