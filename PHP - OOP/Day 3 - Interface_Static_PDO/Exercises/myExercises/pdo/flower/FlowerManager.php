ff2a3753b7e8d760c1fb24b226da888fad1e70ef
Brícia <102607591+bibi-fm@users.noreply.github.com>
2022-08-25 13:19:21 +0200

    add corrected oop pdo flower exercise

--- PHP - OOP/Day 3 - Interface_Static_PDO/Exercises/myExercises/pdo/flower/FlowerManager.php
+++ PHP - OOP/Day 3 - Interface_Static_PDO/Exercises/myExercises/pdo/flower/FlowerManager.php
@@ -2,28 +2,30 @@
 
 class FlowerManager
 {
+    private $pdo;
 
+    public function __construct() {
+        $this->pdo = new PDO('mysql:host=localhost;dbname=flower_db', 'root', '');
+    }
     public function find_all()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=flower_db', 'root', '');
-        $results = $pdo->query('SELECT * FROM flowers');
+        $results = $this->pdo->query('SELECT * FROM flowers');
         $flowers = $results->fetchAll(PDO::FETCH_CLASS, 'Flower');
-        $pdo = null;
+        $this->pdo = null;
         return $flowers;
     }
 
     public function find($id)
     {
         $this->id = $id;
-        $pdo = new PDO('mysql:host=localhost;dbname=flower_db', 'root', '');
         $query = "SELECT * FROM flowers WHERE id = :id";
-        $prep = $pdo->prepare($query);
+        $prep = $this->pdo->prepare($query);
         $prep->bindValue(':id', $id);
         $prep->execute();
 
         $prep->setFetchMode(PDO::FETCH_CLASS, 'Flower');
         $flower = $prep->fetch();
-        $pdo = null;
+        $this->pdo = null;
         return $flower;
     }
 }
