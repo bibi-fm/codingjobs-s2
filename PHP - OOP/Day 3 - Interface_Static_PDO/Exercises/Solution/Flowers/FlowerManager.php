972aad16cf4d2e8390ea2dc8b73ae6fcc093a7e2
Simon <fctinstean@gmail.com>
2022-08-25 11:52:25 +0200

    Flower Manager

--- PHP - OOP/Day 3 - Interface_Static_PDO/Exercises/Solution/Flowers/FlowerManager.php
+++ PHP - OOP/Day 3 - Interface_Static_PDO/Exercises/Solution/Flowers/FlowerManager.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Flower.php';
+
 class FlowerManager
 {
 
@@ -8,9 +10,26 @@ class FlowerManager
         $pdo = new PDO('mysql:host=localhost;dbname=flowers_db', 'root', '');
 
         $results = $pdo->query('SELECT * FROM flowers');
-        $flowers = $results->fetchAll(PDO::FETCH_ASSOC);
+        $flowers = $results->fetchAll(PDO::FETCH_CLASS, 'Flower');
         $pdo = null;
 
         return $flowers;
+    }
+
+    public function find($id)
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=flowers_db', 'root', '');
+
+        $prep = $pdo->prepare('SELECT * 
+        FROM flowers
+        WHERE id = :id');
+        $prep->bindValue(':id', $id);
+        $prep->execute();
+
+        $prep->setFetchMode(PDO::FETCH_CLASS, 'Flower');
+        $flower = $prep->fetch();
+        $pdo = null;
+
+        return $flower;
     }
 }
