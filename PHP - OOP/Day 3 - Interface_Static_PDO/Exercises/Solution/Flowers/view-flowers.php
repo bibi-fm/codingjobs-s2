6921be07c7ef7f8eed75a67cf113c14e65887bf1
Simon <fctinstean@gmail.com>
2022-08-25 09:34:59 +0200

    FlowerManager

---
+++ PHP - OOP/Day 3 - Interface_Static_PDO/Exercises/Solution/Flowers/view-flowers.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flower Page</title>
+</head>
+
+<body>
+    <h2>Flowers List</h2>
+
+    <?php
+    require_once 'FlowerManager.php';
+    $flowerManager = new FlowerManager();
+    $arrayFlowers = $flowerManager->findAll();
+
+    foreach ($arrayFlowers as $flower) {
+        echo 'Name : ' . $flower['name'] . '<br>';
+        echo 'Price : ' . $flower['price'] . '<hr>';
+    }
+    ?>
+
+
+</body>
+
+</html>
