1a183cc983300b6ab59219cbb8eea39e73e4001c
Simon <fctinstean@gmail.com>
2022-08-25 15:01:42 +0200

    Solution

---
+++ PHP - OOP/Day 4 - JSON_Exception/Exercises/Solutions/Flowers/FlowerAPIManager.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'Flower.php';
+require_once 'database.php';
+
+class FlowerAPIManager
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=" . SERVER . ";dbname=" . DB_NAME . ";";
+        $this->pdo = new PDO($dsn, USERNAME, PASSWORD);
+    }
+
+    public function findAll()
+    {
+        $results = $this->pdo->query('SELECT * FROM flowers');
+        $flowers = $results->fetchAll(PDO::FETCH_CLASS, 'Flower');
+        $pdo = null;
+
+        return json_encode($flowers);
+    }
+
+    public function find($id)
+    {
+        $prep = $this->pdo->prepare('SELECT * 
+        FROM flowers
+        WHERE id = :id');
+        $prep->bindValue(':id', $id);
+        $prep->execute();
+
+        $prep->setFetchMode(PDO::FETCH_CLASS, 'Flower');
+        $flower = $prep->fetch();
+        $pdo = null;
+
+        return json_encode($flower);
+    }
+}
