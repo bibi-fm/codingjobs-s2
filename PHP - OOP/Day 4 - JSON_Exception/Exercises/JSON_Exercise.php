7c5729b509fe7019b35cf8a87a7393bbabda4a33
Simon <fctinstean@gmail.com>
2022-08-25 13:56:31 +0200

    JSON Exercise

--- PHP - OOP/Day 4 - JSON_Exception/Exercises/JSON_Exercise.php
+++ PHP - OOP/Day 4 - JSON_Exception/Exercises/JSON_Exercise.php
@@ -2,12 +2,26 @@
 
 /*
 
-In this exercise, we will use flower_db
+In this exercise, we will continue FLOWER EXERCISE
+
+--- Exercise 1
+
+- Step 1 : Create the FlowerAPIManager class.
+It will contains one function findAll().
+
+In this function, return all the flowers in a JSON format.
+
+- Step 2 : Add a new function find($id)
+In this function, return a specific flower as JSON format.
+
+
+--- Exercise 2
 
 - Step 1 :
 
 Create a new page 'new-flower.php'
-This page will contain a form to insert a new flower. Inputs are : name, price and a submit button.
+This page will contain a form to insert a new flower.
+Inputs are : name, price and a submit button.
 
 - Step 2 : 
 
@@ -17,6 +31,10 @@ Add a new method 'insert_flower($name, $price)'
 - Step 3 : 
 
 When we click on the submit button of 'new-flower' page, it should call FlowerManager->insert_flower();
+
+- Step 4 : 
+    Use JSON to send the error or success message.
+    You have to use AJAX to insert the flower.
 
 
 */
