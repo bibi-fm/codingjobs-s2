e9ef574532efedd691b8e0c8799f9961981093fa
Simon <fctinstean@gmail.com>
2022-08-26 14:33:49 +0200

    mail

--- PHP - OOP/Day 5 - Namespace_Mail/Exercises/Solutions/Email/contact.php
+++ PHP - OOP/Day 5 - Namespace_Mail/Exercises/Solutions/Email/contact.php
@@ -13,15 +13,46 @@
 
     <?php
     if (isset($_POST['submitBtn'])) {
+
+        $errors = array();
+
+        if (empty($_POST['name'])) {
+            $errors['name'] = 'Name is mandatory';
+        }
+
+        if (empty($_POST['email'])) {
+            $errors['email'] = 'Email is mandatory';
+        }
+
+        if (empty($_POST['message'])) {
+            $errors['message'] = 'Message is mandatory';
+        }
+
+        if (count($errors) == 0) {
+            require_once 'send_mail.php';
+        }
     }
     ?>
 
     <form method="POST">
-        <input type="text" name="name" placeholder="Full name(*)"><br>
-        <input type="text" name="email" placeholder="Email adress(*)"><br>
-        <input type="text" name="phone" placeholder="Phone number"><br>
-        <input type="text" name="subject" placeholder="Subject"><br>
-        <input type="text" name="message" placeholder="Your message(*)"><br>
+        <input type="text" name="name" placeholder="Full name(*)">
+        <?= (isset($errors['name'])) ? $errors['name'] : ''; ?>
+        <br>
+
+        <input type="text" name="email" placeholder="Email adress(*)">
+        <?= (isset($errors['email'])) ? $errors['email'] : ''; ?>
+        <br>
+
+        <input type="text" name="phone" placeholder="Phone number">
+        <br>
+
+        <input type="text" name="subject" placeholder="Subject">
+        <br>
+
+        <input type="text" name="message" placeholder="Your message(*)">
+        <?= (isset($errors['message'])) ? $errors['message'] : ''; ?>
+        <br>
+
         <small>(*) mandatory</small><br>
         <input type="submit" name="submitBtn" value="Send email">
     </form>
--- PHP - OOP/Day 5 - Namespace_Mail/Exercises/Solutions/Email/send_mail.php
+++ PHP - OOP/Day 5 - Namespace_Mail/Exercises/Solutions/Email/send_mail.php
@@ -13,27 +13,33 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.office365.com';
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'simon-bertrand@live.fr';                     //SMTP username
     $mail->Password   = 'test';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('simon@mywebsite.com', 'Simon');
-    $mail->addAddress('yan@example.net', 'Yan');     //Add a recipient
+    $mail->setFrom('simon-bertrand@live.fr', 'Simon');
+    $mail->addAddress('simon-bertrand@live.fr', 'Makoto');     //Add a recipient
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = $_POST['subject'];
+    $fullMsg = 'Message received from Contact form.<br>
+    Name : ' . $_POST['name'] . '<br> 
+    Phone : ' . $_POST['phone'] . '<br>
+    Message : ' . $_POST['message'] . '<br>
+    <img src="https://i1.sndcdn.com/avatars-000593265963-33fplb-t240x240.jpg" width="100px">';
+
+    $mail->Body    = $fullMsg;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
+    if ($mail->send())
+        echo 'Message has been sent';
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "yo, Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
