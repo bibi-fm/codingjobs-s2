e5d1c6b56882d6614bb96fc495ca948c2ff83c84
Brícia <102607591+bibi-fm@users.noreply.github.com>
2022-08-26 15:19:04 +0200

    add email exercise

---
+++ PHP - OOP/Day 5 - Namespace_Mail/myExercises/send_mail.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output              
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'briciamendes@outlook.com';                     //SMTP username
+    //app password
+    $mail->Password   = 'aneygbhcjxyjhedt';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->CharSet = 'utf-8';
+
+    //Recipients
+    $mail->setFrom('briciamendes@outlook.com', 'Brícia');
+    $mail->addAddress('briciamendes@outlook.com', 'Bibi');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['msg'];
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
