6d61e03cd5653ea73f0801d3e45aad9e91c85f02
Simon <fctinstean@gmail.com>
2022-09-01 16:08:26 +0200

    MVC

--- MVC & Laravel/Day 1 - MVC/Example files/1. MVC/Controller/SongController.php
+++ MVC & Laravel/Day 1 - MVC/Example files/1. MVC/Controller/SongController.php
@@ -1,25 +1,30 @@
 <?php
 
-function handle_songs()
+class SongController
 {
-    // Ask the model the data
-    require_once './Model/SongModel.php';
-    $songs = find_all();
+    public function handle_songs()
+    {
+        // Ask the model the data
+        require_once './Model/SongModel.php';
+        $songModel = new SongModel();
+        $songs = $songModel->find_all();
 
-    // Pass the data to the view
-    require_once './View/SongsView.php';
-}
+        // Pass the data to the view
+        require_once './View/SongsView.php';
+    }
 
-function handle_song($id)
-{
-    // You can check if id is numeric...
-    require_once './Model/SongModel.php';
-    $song = find($id);
+    public function handle_song($id)
+    {
+        // You can check if id is numeric...
+        require_once './Model/SongModel.php';
+        $songModel = new SongModel();
+        $song = $songModel->find($id);
 
-    if ($song != null)
-        require_once './View/SongDetailsView.php';
-    else {
-        $msg = 'Song not available<br>';
-        require_once './View/ErrorView.php';
+        if ($song != null)
+            require_once './View/SongDetailsView.php';
+        else {
+            $msg = 'Song not available<br>';
+            require_once './View/ErrorView.php';
+        }
     }
 }
